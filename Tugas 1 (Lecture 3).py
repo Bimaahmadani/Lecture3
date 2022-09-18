146ccc71244abe51c39a59169d818ca1832378a1
Bimaahmadani <bahmadanidiyaulhaq@gmail.com>
2022-09-18 21:09:58 +0700

    Task 1

---
+++ Tugas 1 (Lecture 3).py
@@ -0,0 +1,10 @@
+print ("hello world")
+answer = input ('greeting :').lower().strip()
+if 'hello' == answer :
+    print ("$0")
+elif 'hello,newman' == answer :
+    print ("$0")
+elif 'h' == answer :
+    print ("$20")
+else:
+    print ("$100")
